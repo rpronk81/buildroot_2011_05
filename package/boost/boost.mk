4bffdf2bb61219999e5afb01e95c3faf1bf9930d
Robert Smigielski <robert.smigielski@bbraun.com>
2016-10-25 16:41:56 -0400

    Boost added with generic support for a cross compiler

--- package/boost/boost.mk
+++ package/boost/boost.mk
@@ -27,7 +27,8 @@ BOOST_ENABLED_LIBS := \
     timer \
 
 define BOOST_BUILD_CMDS
-env -i HOME="$$HOME" LC_CTYPE="$${LC_ALL:-$${LC_CTYPE:-$$LANG}}" PATH="$$PATH:$(GCC_PATH)" USER="$$USER" \
+	env -i HOME="$$HOME" LC_CTYPE="$${LC_ALL:-$${LC_CTYPE:-$$LANG}}" PATH="$$PATH:$(GCC_PATH)" USER="$$USER" \
+	sh -c 'cat $(@D)/../../../package/boost/user-config.jam | sed 's@BR201105@'"$(@D)/../../../"'@'g > $(@D)/tools/build/v2/user-config.jam' 
 	sh -c 'cd $(@D) && \
 	$(@D)/bootstrap.sh && \
 	$(@D)/b2 $(addprefix --with-,$(BOOST_ENABLED_LIBS)) install --prefix="$(@D)/boost.build/"'
