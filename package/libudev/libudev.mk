982b30c39b8d172816a332e2a509717f0baba941
Robert Smigielski <robert.smigielski@bbraun.com>
2017-02-05 17:29:13 -0500

    libudev compiles
    Lots of tricks that are not clean

--- package/libudev/libudev.mk
+++ package/libudev/libudev.mk
@@ -6,7 +6,8 @@
 
 LIBUDEV_VERSION = 199
 LIBUDEV_SOURCE = libudev-$(LIBUDEV_VERSION).tar.gz
-LIBUDEV_SITE = https://github.com/ptdropper/libudev/blob/master/
+#LIBUDEV_SITE = https://github.com/ptdropper/libudev/blob/master/
+LIBUDEV_SITE = https://github.com/ptdropper/libudev/blob/master/libudev-199.tar.gz?raw=true
 LIBUDEV_INSTALL_STAGING = YES
 #LIBUDEV_DEPENDENCIES = host-pkgconf udev libglib2
 #LIBUDEV_DEPENDENCIES = host-intltool host-pkgconf libglib2
