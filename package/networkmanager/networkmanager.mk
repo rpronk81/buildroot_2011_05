5de18e85303d238282ae768300dceb214e6e6872
Robert Smigielski <robert.smigielski@bbraun.com>
2017-02-09 15:02:24 -0500

    Dependency on wireless tools and libnl added

--- package/networkmanager/networkmanager.mk
+++ package/networkmanager/networkmanager.mk
@@ -5,11 +5,13 @@
 #############################################################
 
 NETWORKMANAGER_VERSION = 0.8.1
+# NETWORKMANAGER_SOURCE = networkmanager-0.8.1.tar.gz
 NETWORKMANAGER_SOURCE = NetworkManager-0.8.1.tar.bz2
 NETWORKMANAGER_SITE = https://download.gnome.org/sources/NetworkManager/0.8/
 NETWORKMANAGER_INSTALL_STAGING = YES
 NETWORKMANAGER_INSTALL_TARGET = YES
 # GCC_PATH=$(@D)/../../host/usr/bin
-#NETWORKMANAGER_DEPENDENCIES = libdbus
+NETWORKMANAGER_DEPENDENCIES = wireless_tools libnl
+#NETWORKMANAGER_AUTORECONF = YES
 
 $(eval $(call AUTOTARGETS,package,networkmanager))
