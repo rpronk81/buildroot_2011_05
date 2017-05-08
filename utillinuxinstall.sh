646ced4ee0a0bdf309c1bd754a1b1b6eea3e91ce
Robert Smigielski <ptdropper@gmail.com>
2017-05-08 11:22:20 -0400

    initial build Spacecom2

--- utillinuxinstall.sh
+++ utillinuxinstall.sh
@@ -1,8 +1,8 @@
-export PKG_CONFIG_PATH=/home/robert/50G/br_oct19/buildroot_2011_05/output/build/utillinux-2.20.1/libblkid/:/home/robert/50G/br_oct19/buildroot_2011_05/output/build/utillinux-2.20.1/libuuid/
+export PKG_CONFIG_PATH=/home/robert/50G/buildroot_2011_05/output/spacecom2/build/utillinux-2.20.1/libblkid/:/home/robert/50G/buildroot_2011_05/output/spacecom2/build/utillinux-2.20.1/libuuid/
 
-mkdir ./output/host/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/uuid
-mkdir ./output/host/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/blkid
-cp ./output/build/utillinux-2.20.1/libuuid/src/uuid.h ./output/host/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/uuid/
-cp ./output/build/utillinux-2.20.1/libblkid/src/blkid.h ./output/host/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/blkid/
-cp ./output/build/utillinux-2.20.1/libblkid/src/.libs/libblkid.* ./output/host/usr/arm-unknown-linux-gnueabi/sysroot/usr/lib/
-cp  output/build/utillinux-2.20.1/libuuid/src/.libs/lib* ./output/host/usr/arm-unknown-linux-gnueabi/sysroot/usr/lib/
+mkdir ./output/spacecom2/host/usr/powerpc-unknown-linux-gnu/sysroot/usr/include/uuid
+mkdir ./output/spacecom2/host/usr/powerpc-unknown-linux-gnu/sysroot/usr/include/blkid
+cp ./output/spacecom2/build/utillinux-2.20.1/libuuid/src/uuid.h ./output/spacecom2/host/usr/powerpc-unknown-linux-gnu/sysroot/usr/include/uuid/
+cp ./output/spacecom2/build/utillinux-2.20.1/libblkid/src/blkid.h ./output/spacecom2/host/usr/powerpc-unknown-linux-gnu/sysroot/usr/include/blkid/
+cp ./output/spacecom2/build/utillinux-2.20.1/libblkid/src/.libs/libblkid.* ./output/spacecom2/host/usr/powerpc-unknown-linux-gnu/sysroot/usr/lib/
+cp  output/spacecom2/build/utillinux-2.20.1/libuuid/src/.libs/lib* ./output/spacecom2/host/usr/powerpc-unknown-linux-gnu/sysroot/usr/lib/
