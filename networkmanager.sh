d115fbf477888c8594d9a12412316a163d83508b
Robert Smigielski <ptdropper@gmail.com>
2017-05-09 15:54:10 -0400

    Spacecom2 network manager updates.

--- networkmanager.sh
+++ networkmanager.sh
@@ -1,12 +1,12 @@
-tar -xzf dl/networkmanager-0.8.1.tar.gz -C output/build/networkmanager-0.8.1/
-chmod u+x output/build/networkmanager-0.8.1/configure
- mkdir ./output/host/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/pppd
- cp ./output/build/pppd-2.4.5/pppd/pppd.h  ./output/host/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/pppd/
- cp ./output/build/pppd-2.4.5/pppd/fsm.h  ./output/host/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/pppd/
- cp ./output/build/pppd-2.4.5/pppd/ipcp.h  ./output/host/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/pppd/
- cp ./output/build/pppd-2.4.5/pppd/patchlevel.h  ./output/host/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/
-cp ./output/host/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/nspr/prinit.h ./output/host/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/prinit.h
+tar -xzf dl/networkmanager-0.8.1.tar.gz -C output/spacecom2/build/networkmanager-0.8.1/
+chmod u+x output/spacecom2/build/networkmanager-0.8.1/configure
+ mkdir ./output/spacecom2/host/usr/powerpc-unknown-linux-gnu/sysroot/usr/include/pppd
+ cp ./output/spacecom2/build/pppd-2.4.5/pppd/pppd.h  ./output/spacecom2/host/usr/powerpc-unknown-linux-gnu/sysroot/usr/include/pppd/
+ cp ./output/spacecom2/build/pppd-2.4.5/pppd/fsm.h  ./output/spacecom2/host/usr/powerpc-unknown-linux-gnu/sysroot/usr/include/pppd/
+ cp ./output/spacecom2/build/pppd-2.4.5/pppd/ipcp.h  ./output/spacecom2/host/usr/powerpc-unknown-linux-gnu/sysroot/usr/include/pppd/
+ cp ./output/spacecom2/build/pppd-2.4.5/pppd/patchlevel.h  ./output/spacecom2/host/usr/powerpc-unknown-linux-gnu/sysroot/usr/include/
+cp ./output/spacecom2/host/usr/powerpc-unknown-linux-gnu/sysroot/usr/include/nspr/prinit.h ./output/spacecom2/host/usr/powerpc-unknown-linux-gnu/sysroot/usr/include/prinit.h
 
-cd output/build/networkmanager-0.8.1
-patch -p1 -i ../../../package/networkmanager/nmall.patch
+cd output/spacecom2/build/networkmanager-0.8.1
+patch -p1 -i ../../../../package/networkmanager/nmall.patch
 cd ../../..
--- package/libgudev/libgudev.mk
+++ package/libgudev/libgudev.mk
@@ -5,11 +5,12 @@
 ################################################################################
 
 LIBGUDEV_VERSION = 230
-LIBGUDEV_SOURCE = libgudev-$(LIBGUDEV_VERSION).tar.xz
+LIBGUDEV_SOURCE = libgudev-$(LIBGUDEV_VERSION).tar.gz
+#LIBGUDEV_SOURCE = libgudev-$(LIBGUDEV_VERSION).tar.xz
 LIBGUDEV_SITE = http://ftp.gnome.org/pub/GNOME/sources/libgudev/$(LIBGUDEV_VERSION)
 LIBGUDEV_INSTALL_STAGING = YES
 # LIBGUDEV_DEPENDENCIES = host-pkgconf udev libglib2
-LIBGUDEV_DEPENDENCIES = pkg-config udev libglib2
+LIBGUDEV_DEPENDENCIES = pkg-config libudev udev libglib2
 LIBGUDEV_LICENSE = LGPLv2.1+
 LIBGUDEV_LICENSE_FILES = COPYING
 
