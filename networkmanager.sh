b18c61a2e562914f3f2432f5a11cc9255c541b23
Robert Smigielski <ptdropper@gmail.com>
2017-05-23 16:14:08 -0400

    Clean up

--- networkmanager.sh
+++ networkmanager.sh
@@ -1,5 +1,7 @@
-mkdir ./output/host/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/pppd
-cp ./output/build/pppd-2.4.5/pppd/pppd.h  ./output/host/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/pppd/
-cp ./output/build/pppd-2.4.5/pppd/fsm.h  ./output/host/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/pppd/
-cp ./output/build/pppd-2.4.5/pppd/ipcp.h  ./output/host/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/pppd/
-cp ./output/build/pppd-2.4.5/pppd/patchlevel.h  ./output/host/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/
+export BR2_HOST_DIR=/opt/buildroot-2011.05
+
+mkdir ${BR2_HOST_DIR}/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/pppd
+cp ./output/build/pppd-2.4.5/pppd/pppd.h  ${BR2_HOST_DIR}/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/pppd/
+cp ./output/build/pppd-2.4.5/pppd/fsm.h  ${BR2_HOST_DIR}/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/pppd/
+cp ./output/build/pppd-2.4.5/pppd/ipcp.h  ${BR2_HOST_DIR}/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/pppd/
+cp ./output/build/pppd-2.4.5/pppd/patchlevel.h  ${BR2_HOST_DIR}/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/
--- utillinuxinstall.sh
+++ utillinuxinstall.sh
@@ -1,8 +1,10 @@
 export PKG_CONFIG_PATH=/home/robert/50G/br_oct19/buildroot_2011_05/output/build/utillinux-2.20.1/libblkid/:/home/robert/50G/br_oct19/buildroot_2011_05/output/build/utillinux-2.20.1/libuuid/
 
-mkdir ./output/host/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/uuid
-mkdir ./output/host/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/blkid
-cp ./output/build/utillinux-2.20.1/libuuid/src/uuid.h ./output/host/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/uuid/
-cp ./output/build/utillinux-2.20.1/libblkid/src/blkid.h ./output/host/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/blkid/
-cp ./output/build/utillinux-2.20.1/libblkid/src/.libs/libblkid.* ./output/host/usr/arm-unknown-linux-gnueabi/sysroot/usr/lib/
-cp  output/build/utillinux-2.20.1/libuuid/src/.libs/lib* ./output/host/usr/arm-unknown-linux-gnueabi/sysroot/usr/lib/
+export BR2_HOST_DIR=/opt/buildroot-2011.05
+
+mkdir ${BR2_HOST_DIR}/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/uuid
+mkdir ${BR2_HOST_DIR}/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/blkid
+cp ./output/build/utillinux-2.20.1/libuuid/src/uuid.h ${BR2_HOST_DIR}/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/uuid/
+cp ./output/build/utillinux-2.20.1/libblkid/src/blkid.h ${BR2_HOST_DIR}/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/blkid/
+cp ./output/build/utillinux-2.20.1/libblkid/src/.libs/libblkid.* ${BR2_HOST_DIR}/usr/arm-unknown-linux-gnueabi/sysroot/usr/lib/
+cp  output/build/utillinux-2.20.1/libuuid/src/.libs/lib* ${BR2_HOST_DIR}/usr/arm-unknown-linux-gnueabi/sysroot/usr/lib/
