56a90db10c1c7deba95cacf0e1186759b6afcdb0
Robert Smigielski <ptdropper@gmail.com>
2017-09-05 09:08:28 -0400

    json dbus bridge add the libfcgi

--- package/libfcgi/libfcgi.mk
+++ package/libfcgi/libfcgi.mk
@@ -7,7 +7,9 @@ LIBFCGI_VERSION = 2.4.0
 LIBFCGI_SOURCE = fcgi-$(LIBFCGI_VERSION).tar.gz
 LIBFCGI_SITE = https://sourceforge.net/projects/slackbuildsdirectlinks/files/fcgi/
 
-LIBFCGI_INSTALL_STAGING = YES
+# LIBFCGI_INSTALL_STAGING = YES
+# LIBFCGI_CONF_OPT = --without-examples
+
 LIBFCGI_CONF_ENV = LDFLAGS="$(TARGET_LDFLAGS) -lm"
 
 $(eval $(call AUTOTARGETS,package,libfcgi))
