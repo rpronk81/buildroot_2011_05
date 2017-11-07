f4fa933b64cab82844296fe73c7103affb5cfd84
Robert Smigielski <ptdropper@gmail.com>
2017-11-07 08:20:55 -0500

    Install headers

--- package/json-c/json-c.mk
+++ package/json-c/json-c.mk
@@ -8,4 +8,9 @@ JSON_C_VERSION = 0.10-20120530
 JSON_C_SITE = https://github.com/json-c/json-c/archive
 JSON_C_INSTALL_STAGING = YES
 
+define JSON_C_INSTALL_STAGING_CMDS
+   cp output/build/json-c-0.10-20120530/json_object_iterator.h $(BR2_HOST_DIR)/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/json/
+
+endef
+
 $(eval $(call AUTOTARGETS,package,json-c))
