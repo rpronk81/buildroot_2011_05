4d5bc8b85cc7a3c801caebfd6aeebb0f5b3f3cba
Robert Smigielski <ptdropper@gmail.com>
2017-12-13 15:51:08 -0500

    json build issue corrected

--- package/json-c/json-c.mk
+++ package/json-c/json-c.mk
@@ -7,10 +7,13 @@
 JSON_C_VERSION = 0.10-20120530
 JSON_C_SITE = https://github.com/json-c/json-c/archive
 JSON_C_INSTALL_STAGING = YES
+JSON_C_INSTALL_TARGET = YES
+# JSON_C_AUTORECONF = YES
 
 define JSON_C_INSTALL_STAGING_CMDS
-   cp output/build/json-c-0.10-20120530/json_object_iterator.h $(BR2_HOST_DIR)/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/json/
-
+   mkdir $(BR2_HOST_DIR)/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/json/
+   cp output/build/json-c-0.10-20120530/*.h $(BR2_HOST_DIR)/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/json/
+   cp -p output/target/usr/lib/libjson.*  /opt/buildroot-2011.05/usr/arm-unknown-linux-gnueabi/sysroot/usr/lib
 endef
 
 $(eval $(call AUTOTARGETS,package,json-c))
