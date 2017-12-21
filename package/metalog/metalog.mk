283cbb3ca314c3c10ca1bee112d891a3af56ff3c
Robert Smigielski <ptdropper@gmail.com>
2017-12-21 10:51:19 -0500

    Corrected the download directory
    Previous download path has gone away.

--- package/metalog/metalog.mk
+++ package/metalog/metalog.mk
@@ -5,7 +5,9 @@
 ##############################################################################
 METALOG_VERSION = 1
 METALOG_SOURCE = metalog-$(METALOG_VERSION).tar.gz
-METALOG_SITE =  https://sourceforge.net/projects/metalog/files/metalog-$(METALOG_VERSION).tar.lzma/download
+#METALOG_SITE =  https://sourceforge.net/projects/metalog/files/metalog-$(METALOG_VERSION).tar.lzma/download
+METALOG_SITE =  https://github.com/ptdropper/metalog/raw/master/
+
 
 METALOG_INSTALL_STAGING = YES
 METALOG_AUTORECONF = YES
