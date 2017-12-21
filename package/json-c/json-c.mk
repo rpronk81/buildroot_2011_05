e6425d4b505aa0f86bffffa6cb44c65eb0badba6
Robert Smigielski <ptdropper@gmail.com>
2017-12-21 11:09:14 -0500

    Correction to optional tool chain installation
    If the files are already installed in the tool chain, then this
    commit adds logic to only create the path if needed.

--- package/json-c/json-c.mk
+++ package/json-c/json-c.mk
@@ -8,12 +8,14 @@ JSON_C_VERSION = 0.10-20120530
 JSON_C_SITE = https://github.com/json-c/json-c/archive
 JSON_C_INSTALL_STAGING = YES
 JSON_C_INSTALL_TARGET = YES
-# JSON_C_AUTORECONF = YES
 
 define JSON_C_INSTALL_STAGING_CMDS
-   mkdir $(BR2_HOST_DIR)/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/json/
+   @if [ ! -d "$(BR2_HOST_DIR)/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/json" ]; \
+   then \
+      mkdir "$(BR2_HOST_DIR)/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/json" ; \
+   fi
    cp output/build/json-c-0.10-20120530/*.h $(BR2_HOST_DIR)/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/json/
-   cp -p output/target/usr/lib/libjson.*  /opt/buildroot-2011.05/usr/arm-unknown-linux-gnueabi/sysroot/usr/lib
+   cp -p output/build/json-c-0.10-20120530/libjson.*  /opt/buildroot-2011.05/usr/arm-unknown-linux-gnueabi/sysroot/usr/lib
 endef
 
 $(eval $(call AUTOTARGETS,package,json-c))
