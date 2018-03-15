344b148970681973de9a8deb018b5f68227c3d54
Robert Smigielski <ptdropper@gmail.com>
2018-03-15 15:59:35 -0400

    Reconfigure json so that the symbols are provided to json-dbus-bridge

--- package/json-c/json-c.mk
+++ package/json-c/json-c.mk
@@ -8,6 +8,6 @@ JSON_C_VERSION = 0.10-20120530
 JSON_C_SITE = https://github.com/json-c/json-c/archive
 JSON_C_INSTALL_STAGING = YES
 JSON_C_INSTALL_TARGET = YES
-# JSON_C_AUTORECONF = YES
+JSON_C_AUTORECONF = YES
 
 $(eval $(call AUTOTARGETS,package,json-c))
