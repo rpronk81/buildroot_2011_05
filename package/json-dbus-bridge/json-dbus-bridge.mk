d54d78f2e21970bd6f3edde9772908a4c7e291a3
Robert Smigielski <ptdropper@gmail.com>
2018-03-15 13:50:52 -0400

    json issues found and corrected by Tim Schmidt

--- package/json-dbus-bridge/json-dbus-bridge.mk
+++ package/json-dbus-bridge/json-dbus-bridge.mk
@@ -4,11 +4,11 @@
 #
 #############################################################
 
-JSON_DBUS_BRIDGE_VERSION = 1.0.0
-JSON_DBUS_BRIDGE_SITE = http://public.pengutronix.de/software/json-dbus-bridge
-JSON_DBUS_BRIDGE_SOURCE = json-dbus-bridge-$(JSON_DBUS_BRIDGE_VERSION).tar.bz2
+JSON_DBUS_BRIDGE_VERSION = 1.1.5
+JSON_DBUS_BRIDGE_SITE = https://github.com/elrafoon/json-dbus-bridge/archive/
+JSON_DBUS_BRIDGE_SOURCE = v$(JSON_DBUS_BRIDGE_VERSION).tar.gz
 
-JSON_DBUS_BRIDGE_DEPENDENCIES = libfcgi libevent
+JSON_DBUS_BRIDGE_DEPENDENCIES = libfcgi libevent libjson
 
 JSON_DBUS_BRIDGE_AUTORECONF = YES
 
@@ -20,8 +20,7 @@ endif
 
 define JSON_DBUS_BRIDGE_CPY_JSON_C_LIB
 	mkdir -p $(CUSTOM_TOOLCHAIN_PATH)/sysroot/usr/include/json
-	cp $(DL_DIR)/../output/build/json-c-0.10-20120530/*.h $(CUSTOM_TOOLCHAIN_PATH)/sysroot/usr/include/json/
-	cp -p $(DL_DIR)/../output/target/usr/lib/libjson.*  $(CUSTOM_TOOLCHAIN_PATH)/sysroot/usr/lib
+	cp $(DL_DIR)/../output/target/usr/lib/libjson.*  $(CUSTOM_TOOLCHAIN_PATH)/sysroot/usr/lib
 endef
 
 JSON_DBUS_BRIDGE_POST_EXTRACT_HOOKS += JSON_DBUS_BRIDGE_CPY_JSON_C_LIB
