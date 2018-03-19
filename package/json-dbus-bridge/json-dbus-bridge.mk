2ce01f92a48522a99c90fbc5c8b0e5f8c1cf11a6
Tim Schmidt <32720525+TimSchmidt15@users.noreply.github.com>
2018-03-19 06:12:48 -0400

    update json-dbus-bridge version

--- package/json-dbus-bridge/json-dbus-bridge.mk
+++ package/json-dbus-bridge/json-dbus-bridge.mk
@@ -4,9 +4,9 @@
 #
 #############################################################
 
-JSON_DBUS_BRIDGE_VERSION = 1.0.0
-JSON_DBUS_BRIDGE_SITE = http://public.pengutronix.de/software/json-dbus-bridge
-JSON_DBUS_BRIDGE_SOURCE = json-dbus-bridge-$(JSON_DBUS_BRIDGE_VERSION).tar.bz2
+JSON_DBUS_BRIDGE_VERSION = 1.1.5
+JSON_DBUS_BRIDGE_SITE = https://github.com/elrafoon/json-dbus-bridge/archive/
+JSON_DBUS_BRIDGE_SOURCE = v$(JSON_DBUS_BRIDGE_VERSION).tar.gz
 
 JSON_DBUS_BRIDGE_DEPENDENCIES = libfcgi libevent
 
@@ -15,7 +15,7 @@ JSON_DBUS_BRIDGE_AUTORECONF = YES
 define JSON_DBUS_BRIDGE_CPY_JSON_C_LIB
 	mkdir -p $(BR2_HOST_DIR)/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/json
 	cp $(DL_DIR)/../output/build/json-c-0.10-20120530/*.h $(BR2_HOST_DIR)/usr/arm-unknown-linux-gnueabi/sysroot/usr/include/json/
-	cp -p $(DL_DIR)/../output/target/usr/lib/libjson.*  $(BR2_HOST_DIR)/usr/arm-unknown-linux-gnueabi/sysroot/usr/lib
+	cp $(DL_DIR)/../output/target/usr/lib/libjson.*  $(BR2_HOST_DIR)/usr/arm-unknown-linux-gnueabi/sysroot/usr/lib
 endef
 
 JSON_DBUS_BRIDGE_POST_EXTRACT_HOOKS += JSON_DBUS_BRIDGE_CPY_JSON_C_LIB
