becf8509874fd5466c75bc34b9f0d3be1b134042
Tim Schmidt <32720525+TimSchmidt15@users.noreply.github.com>
2018-03-19 09:25:17 -0400

    fixed wrong networkmanager url to sources

--- package/networkmanager/networkmanager.mk
+++ package/networkmanager/networkmanager.mk
@@ -6,7 +6,7 @@
 NETWORKMANAGER_VERSION = 0.8.1
 NETWORKMANAGER_VERSION_MAJOR=0.8
 NETWORKMANAGER_SOURCE = NetworkManager-$(NETWORKMANAGER_VERSION).git20100811.tar
-NETWORKMANAGER_SITE = https://github.com/ptdropper/NetworkManager/blob/master/
+NETWORKMANAGER_SITE = https://github.com/ptdropper/NetworkManager/raw/master/
 
 NETWORKMANAGER_INSTALL_STAGING = YES
 NETWORKMANAGER_DEPENDENCIES = host-pkg-config udev dbus-glib libnl wireless_tools gnutls utillinux polkit pppd
