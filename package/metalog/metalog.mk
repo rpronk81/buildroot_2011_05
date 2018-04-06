8c236cfb1ee0cff27b85da08421cb14dd59c7a49
Robert Smigielski <robert.smigielski@bbraunusa.com>
2018-04-06 10:59:04 -0400

    Corrected download site and dependency

--- package/metalog/metalog.mk
+++ package/metalog/metalog.mk
@@ -5,9 +5,8 @@
 ##############################################################################
 METALOG_VERSION = 1
 METALOG_SOURCE = metalog-$(METALOG_VERSION).tar.gz
-#METALOG_SITE =  https://sourceforge.net/projects/metalog/files/metalog-$(METALOG_VERSION).tar.lzma/download
 METALOG_SITE =  https://github.com/ptdropper/metalog/raw/master/
-
+METALOG_DEPENDENCIES = pcre
 
 METALOG_INSTALL_STAGING = YES
 METALOG_AUTORECONF = YES
